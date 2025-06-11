7fd6fac58989c5e731cf06915b21e27a70e1e028
Richard Akrong <97akrichard@gmail.com>
2025-06-11 03:00:54 +0000

    fic

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Footer from './components/Footer/Footer'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
     <Footer />
     <ScrollToTop />
+    <App />
   </React.StrictMode>,
 )
