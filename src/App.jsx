4578cc72cf5ae730dbed133a50f85f8916270eaf
Richard Akrong <97akrichard@gmail.com>
2025-06-11 02:26:21 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
-
 function App() {
   return (
     <div className="App">
