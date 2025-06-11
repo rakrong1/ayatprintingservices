2780d95240ac48691734f40842b4585b8d02f18f
Richard Akrong <97akrichard@gmail.com>
2025-06-11 03:02:44 +0000

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <App />
     <Footer />
     <ScrollToTop />
-    <App />
   </React.StrictMode>,
 )
